28b5d0b66212e332861d11ca9dfe619b19371264
PratikVR <150180168+PratikVR@users.noreply.github.com>
2024-02-20 05:38:40 +0000

    loader manipulation using js has been done, we've got a new navigation button animation!

--- main.js
+++ main.js
@@ -28,13 +28,51 @@ navbar.forEach(element=>{
 function showHomePage(){
     container.style.setProperty('--rotate-x','0deg');
     container.style.setProperty('--rotate-y','0deg');
+    setTimeout(() => {
+        resetLoaders();    
+    }, 100);
 }
 function showAboutMePage(){
     container.style.setProperty('--rotate-x','0deg');
     container.style.setProperty('--rotate-y','90deg');
+    setTimeout(() => {
+        setLoaders();    
+    }, 500);
 }
 function showContactMePage(){
     container.style.setProperty('--rotate-x','-90deg');
     container.style.setProperty('--rotate-y','0deg');
+    setTimeout(() => {
+        resetLoaders();    
+    }, 100);
 }
 
+const jsLoader = document.querySelector('#js-loader');
+const htmlLoader = document.querySelector('#html-loader');
+const css3Loader = document.querySelector('#css-loader');
+const javaLoader = document.querySelector('#java-loader');
+const pythonLoader = document.querySelector('#python-loader');
+function setLoaders(){
+    jsLoader.style.width="95%";
+    jsLoader.innerHTML = '95%';
+    htmlLoader.style.width="92%";
+    htmlLoader.innerHTML = '92%';
+    css3Loader.style.width="95%";
+    css3Loader.innerHTML = '95%'
+    javaLoader.style.width="71%";
+    javaLoader.innerHTML = "71%";
+    pythonLoader.style.width="67%";
+    pythonLoader.innerHTML = "67%";
+}
+function resetLoaders(){
+    jsLoader.style.width="0%";
+    jsLoader.innerHTML = '95%';
+    htmlLoader.style.width="0%";
+    htmlLoader.innerHTML = '92%';
+    css3Loader.style.width="0%";
+    css3Loader.innerHTML = '95%'
+    javaLoader.style.width="0%";
+    javaLoader.innerHTML = "71%";
+    pythonLoader.style.width="0%";
+    pythonLoader.innerHTML = "67%";
+}
